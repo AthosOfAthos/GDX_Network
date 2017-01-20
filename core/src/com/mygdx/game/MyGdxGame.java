41b367149be0f4b058cf0fbd7b709d2cef04f087
athosofathos <athosofathos@gmail.com>
2017-01-20 09:16:05 -0600

    Player can move!

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -16,8 +17,17 @@ public class MyGdxGame extends ApplicationAdapter {
 	    playerPaddle = new Paddle(100,100,10);
 	}
 
+	private void loop() {
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            playerPaddle.moveUp();
+        } else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            playerPaddle.moveDown();
+        }
+    }
+
 	@Override
 	public void render () {
+	    loop();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
