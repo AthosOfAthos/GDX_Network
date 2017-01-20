cfec543dea0a21d935064503296e2a56f1c35978
athosofathos <athosofathos@gmail.com>
2017-01-20 09:27:22 -0600

    Added resize function

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,19 +3,32 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MyGdxGame extends ApplicationAdapter {
+    int width, height;
+
 	SpriteBatch batch;
 	Paddle playerPaddle;
 	
 	@Override
 	public void create () {
+	    width = Gdx.graphics.getWidth();
+	    height = Gdx.graphics.getHeight();
 	    batch = new SpriteBatch();
 	    playerPaddle = new Paddle(100,100,100);
 	}
+
+	@Override
+    public void resize(int getWidth, int getHeight) {
+	    width = getWidth;
+	    height = getHeight;
+    }
 
 	private void loop() {
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
